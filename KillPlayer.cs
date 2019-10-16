375bbc7923ad5992c375ebca7b86daff59647f17
JackFritz <56080401+JackFritz@users.noreply.github.com>
2019-10-16 11:35:03 -0400

    Add files via upload

---
+++ KillPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillPlayer : MonoBehaviour
+{
+    public Text tellPlayer;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "dead")
+        {
+            tellPlayer.text = "Press R to Restart";
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Move1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move1 : MonoBehaviour
+{
+    float speed = -5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+
+        if (transform.position.y < -2.1f)
+        {
+            transform.position = new Vector2(7.5f, 1.35f);
+        }
+    }
+}
